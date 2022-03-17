1ef4038cd9b65f2f8048c4a954d240bafe490095
Shady-Rouk <faroukbalogun@techexchange.in>
2022-03-17 16:18:50 +0000

    adding our names

--- flask-template/app.py
+++ flask-template/app.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#Lab Partners: Josue Estrada, Farouk Balogun
+
 # ---- YOUR APP STARTS HERE ----
 # -- Import section --
 from flask import Flask
